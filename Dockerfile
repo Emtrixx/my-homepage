5de10d92f671493d231570c781b54bfb79bdcdd7
Jesse Günzl <20597157+Emtrixx@users.noreply.github.com>
2022-01-26 02:52:31 +0100

    pdf decrypt functional, configured dockerimage for qpdf

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,14 @@
 FROM node:14
+WORKDIR /home
+RUN apt-get update \
+      && apt-get install -y sudo
+RUN apt install build-essential
+RUN apt install zlib1g-dev
+RUN apt install  libjpeg-dev
+RUN git clone https://github.com/qpdf/qpdf.git
+RUN cd qpdf/ && ./configure && make && make install
+RUN export LD_LIBRARY_PATH=/usr/local/lib
+RUN ldconfig
 WORKDIR /home/node/app
 COPY package.json .
 RUN npm install
--- routes/pdfEditor.js
+++ routes/pdfEditor.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const isLoggedIn = require('../utils/isLoggedIn');
+const fs = require('fs')
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const { exec } = require('child_process');
@@ -8,20 +9,30 @@ const { exec } = require('child_process');
 router.route('/')
 .post(isLoggedIn ,upload.single('pdf'), (req, res) => {
     console.log(req.file)
-    const { path } = req.file;
-    exec(`qpdf --decrypt ${path} output.pdf`, (err, stdout, stderr) => {
+    const { path, originalname } = req.file;
+    const newPath = `pdf/decrypt_${originalname}`
+    // TODO Put path in env
+    exec(`qpdf --decrypt /home/node/app/${path} /home/node/app/${newPath}`, (err, stdout, stderr) => {
       if (err) {
-        console.log('fail')
+        console.log(err)
         return;
       }
       console.log(`stdout: ${stdout}`);
       console.log(`stderr: ${stderr}`);
+
+      if (fs.existsSync(newPath)) {
+          res.contentType("application/pdf");
+          fs.createReadStream(newPath).pipe(res)
+      } else {
+          res.status(500)
+          console.log('File not found')
+          res.send('File not found')
+      }
     });
 })
 
 .get(isLoggedIn,(req, res) => {
     res.render('pdfEditor/index');
 })
-
 
  module.exports = router;
